9df1750f0cbbdb45d611dbab5b7f74e23a7bb878
Alexander Nemish <anemish@gmail.com>
2023-11-14 13:32:42 +0100

    HTLC contract

--- contract.scala
+++ contract.scala
@@ -8,8 +8,10 @@ import io.bullet.borer.Cbor
 import scalus.*
 import scalus.Compile
 import scalus.Compiler.compile
+import scalus.Compiler.fieldAsData
 import scalus.builtins.Builtins
 import scalus.builtins.ByteString
+import scalus.ledger.api.v1.FromDataInstances.given
 import scalus.ledger.api.v2.FromDataInstances.given
 import scalus.ledger.api.v2.ScriptPurpose.*
 import scalus.ledger.api.v2.*
@@ -26,8 +28,11 @@ import scalus.uplc.Data
 import scalus.uplc.Data.fromData
 import scalus.uplc.Program
 import scalus.uplc.ProgramFlatCodec
+import scalus.uplc.FromDataInstances.given
 import scalus.uplc.Term
 import scalus.utils.Utils
+import scalus.ledger.api.v1.POSIXTime
+import scalus.ledger.api.v1.Extended
 
 @Compile
 object BondContract {
@@ -40,14 +45,42 @@ object BondContract {
   ) = (datum: Unit, redeemer: Unit, ctxData: Data) => {
     throw new Exception("TODO")
   }
+
+  /*
+   * HTLC contract validator
+   */
+  def makeHtlcContractValidator(clientPubKeyHash: Data, expiration: POSIXTime, hash: ByteString) =
+    (datum: Data, redeemer: Data, ctxData: Data) => {
+      val validPreimage = Builtins.equalsByteString(hash, Builtins.sha2_256(Builtins.unsafeDataAsB(redeemer)))
+      val expired = {
+        val txInfoData = fieldAsData[ScriptContext](_.txInfo)(ctxData)
+        val signatoriesData = fieldAsData[TxInfo](_.signatories)(txInfoData)
+        val txtime = fromData[POSIXTimeRange]( fieldAsData[TxInfo](_.validRange)(txInfoData) )
+        txtime.from.extended match
+          case Extended.Finite(txtime) =>
+            val expired = expiration < txtime
+            val signedByClient = {
+              val signaturePubKeyHashData = Builtins.unsafeDataAsList(signatoriesData).head
+              Builtins.equalsData(signaturePubKeyHashData, clientPubKeyHash)
+            }
+            expired && signedByClient
+          case _ => false
+      }
+      if validPreimage || expired then () else throw new Exception()
+  }
 }
 
 @main def main() = {
-  val compiledScript = compile(BondContract.bondContractValidator)
-  val validator = compiledScript.toUplc(generateErrorTraces = true)
-  val program = Program((2, 0, 0), validator)
-  println(compiledScript.pretty.render(100))
-  println(program.doubleCborHex)
+  val compiledBondScript = compile(BondContract.bondContractValidator)
+  val bondValidator = compiledBondScript.toUplc(generateErrorTraces = true)
+  val bondProgram = Program((2, 0, 0), bondValidator)
+  val compiledHtlcScript = compile(BondContract.makeHtlcContractValidator)
+  val htlcValidator = compiledHtlcScript.toUplc(generateErrorTraces = true)
+  val htlcProgram = Program((2, 0, 0), htlcValidator)
+  println(compiledBondScript.pretty.render(100))
+  println(bondProgram.doubleCborHex)
+  println(compiledHtlcScript.pretty.render(100))
+  println(htlcProgram.doubleCborHex)
+  println(s"bondProgram size: ${bondProgram.doubleCborEncoded.size}")
+  println(s"htlcProgram size: ${htlcProgram.doubleCborEncoded.size}")
 }
-
-
